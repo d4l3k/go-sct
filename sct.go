6889d2d4ef9ab255ffa041101a12de7d0e3f2a57
Tristan Rice <rice@fn.lc>
2015-11-18 20:14:25 -0800

    Experimental dual screen support

--- sct.go
+++ sct.go
@@ -15,9 +15,6 @@ package sct
 // #include <X11/Xatom.h>
 // #include <X11/extensions/Xrandr.h>
 // #include <X11/extensions/Xrender.h>
-// int DefaultScreenMacro(Display * dpy) {
-//   return DefaultScreen(dpy);
-// }
 // Window RootWindowMacro(Display * dpy, int scr) {
 //   return RootWindow(dpy, scr);
 // }
@@ -26,6 +23,9 @@ package sct
 // }
 // void ushortSet(ushort * s, int k, ushort v) {
 //	 s[k] = (ushort)v;
+// }
+// int screenCount(Display * dpy) {
+//   return XScreenCount(dpy);
 // }
 import "C"
 import "unsafe"
@@ -61,33 +61,35 @@ var whitepoints = []color{
 // SetColorTemp changes the Xrandr colors to reflect the specified color temperature.
 func SetColorTemp(temp int) {
 	dpy := C.XOpenDisplay(nil)
-	screen := C.DefaultScreenMacro(dpy)
-	root := C.RootWindowMacro(dpy, screen)
+	screenCount := C.screenCount(dpy)
+	for screen := C.int(0); screen < screenCount; screen++ {
+		root := C.RootWindowMacro(dpy, screen)
 
-	res := C.XRRGetScreenResourcesCurrent(dpy, root)
+		res := C.XRRGetScreenResourcesCurrent(dpy, root)
 
-	if temp < 1000 || temp > 10000 {
-		temp = 6500
-	}
-	temp -= 1000
-	ratio := float64((temp-1000)%500) / 500.0
-	point := whitepoints[temp/500]
-	gammar := point.r*(1-ratio) + point.r*ratio
-	gammag := point.g*(1-ratio) + point.g*ratio
-	gammab := point.b*(1-ratio) + point.b*ratio
-
-	for c := C.int(0); c < res.ncrtc; c++ {
-		crtcxid := C.crtcxid(res.crtcs, c)
-
-		size := C.XRRGetCrtcGammaSize(dpy, crtcxid)
-		crtc_gamma := C.XRRAllocGamma(size)
-		for i := C.int(0); i < size; i++ {
-			g := 65535.0 * float64(i) / float64(size)
-			C.ushortSet(crtc_gamma.red, i, C.ushort(g*gammar))
-			C.ushortSet(crtc_gamma.green, i, C.ushort(g*gammag))
-			C.ushortSet(crtc_gamma.blue, i, C.ushort(g*gammab))
+		if temp < 1000 || temp > 10000 {
+			temp = 6500
 		}
-		C.XRRSetCrtcGamma(dpy, crtcxid, crtc_gamma)
-		C.XFree(unsafe.Pointer(crtc_gamma))
+		temp -= 1000
+		ratio := float64((temp-1000)%500) / 500.0
+		point := whitepoints[temp/500]
+		gammar := point.r*(1-ratio) + point.r*ratio
+		gammag := point.g*(1-ratio) + point.g*ratio
+		gammab := point.b*(1-ratio) + point.b*ratio
+
+		for c := C.int(0); c < res.ncrtc; c++ {
+			crtcxid := C.crtcxid(res.crtcs, c)
+
+			size := C.XRRGetCrtcGammaSize(dpy, crtcxid)
+			crtc_gamma := C.XRRAllocGamma(size)
+			for i := C.int(0); i < size; i++ {
+				g := 65535.0 * float64(i) / float64(size)
+				C.ushortSet(crtc_gamma.red, i, C.ushort(g*gammar))
+				C.ushortSet(crtc_gamma.green, i, C.ushort(g*gammag))
+				C.ushortSet(crtc_gamma.blue, i, C.ushort(g*gammab))
+			}
+			C.XRRSetCrtcGamma(dpy, crtcxid, crtc_gamma)
+			C.XFree(unsafe.Pointer(crtc_gamma))
+		}
 	}
 }
