834b11814d592c34c1243e6fe564e6849814553f
Chris Howey <chris@howey.me>
2015-11-21 07:41:18 -0600

    Add a "timed" mode to command

--- sct/main.go
+++ sct/main.go
@@ -17,8 +17,12 @@ import (
 var dayTemp = flag.Int("dayTemp", 6500, "The color temperature during the day.")
 var nightTemp = flag.Int("nightTemp", 3000, "The color temperature during the day.")
 var daemon = flag.Bool("d", true, "run app as a daemon")
+var mode = flag.String("mode", "geoip", "Mode of daemon (geoip or timed). Timed mode uses specified sunrise-time, midday-time, and sunset-time.")
+var sunriseTimeStr = flag.String("sunrise-time", "06:30", "Sunrise time (HH:MM)")
+var sunsetTimeStr = flag.String("sunset-time", "21:00", "Sunset time (HH:MM)")
+var middayTimeStr = flag.String("midday-time", "14:00", "Mid day (brightest) time (HH:MM)")
 
-func monitorTime() {
+func monitorGeo() {
 	log.Printf("Fetching location...")
 	geo, err := geoip.LookupIP("")
 	if err != nil {
@@ -47,6 +51,49 @@ func monitorTime() {
 	}
 }
 
+func monitorTime() {
+	var sunriseTime, sunsetTime, middayTime time.Time
+	var monitorTemperature int
+	monitorTemperature = 6500
+
+	for {
+		curTime := time.Now()
+		midnight := time.Date(curTime.Year(), curTime.Month(), curTime.Day(), 0, 0, 0, 0, time.Local)
+
+		if parseTime, perr := time.ParseInLocation("2006-01-02 15:04", midnight.Format("2006-01-02 ")+*sunriseTimeStr, time.Local); perr != nil {
+			log.Fatal(perr)
+		} else {
+			sunriseTime = parseTime
+		}
+		if parseTime, perr := time.ParseInLocation("2006-01-02 15:04", midnight.Format("2006-01-02 ")+*sunsetTimeStr, time.Local); perr != nil {
+			log.Fatal(perr)
+		} else {
+			sunsetTime = parseTime
+		}
+		if parseTime, perr := time.ParseInLocation("2006-01-02 15:04", midnight.Format("2006-01-02 ")+*middayTimeStr, time.Local); perr != nil {
+			log.Fatal(perr)
+		} else {
+			middayTime = parseTime
+		}
+
+		if curTime.After(sunriseTime) && curTime.Before(sunsetTime) {
+			if curTime.Before(middayTime) {
+				elapsedDuration := curTime.Sub(sunriseTime)
+				ratio := float64(elapsedDuration) / float64(middayTime.Sub(sunriseTime))
+				monitorTemperature = int(float64(*nightTemp)*(1-ratio) + float64(*dayTemp)*ratio)
+			} else {
+				elapsedDuration := curTime.Sub(middayTime)
+				ratio := float64(elapsedDuration) / float64(sunsetTime.Sub(middayTime))
+				monitorTemperature = int(float64(*dayTemp)*(1-ratio) + float64(*nightTemp)*ratio)
+			}
+		} else {
+			monitorTemperature = *nightTemp
+		}
+		sct.SetColorTemp(monitorTemperature)
+		time.Sleep(10 * time.Minute)
+	}
+}
+
 func main() {
 	flag.Parse()
 	args := flag.Args()
@@ -69,7 +116,12 @@ func main() {
 			log.Println("Launched background process... pid", cmd.Process.Pid)
 			os.Exit(0)
 		} else {
-			monitorTime()
+			switch *mode {
+			case "timed":
+				monitorTime()
+			default:
+				monitorGeo()
+			}
 		}
 	}
 }
